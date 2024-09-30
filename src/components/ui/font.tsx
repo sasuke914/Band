6e5eafedfa6905158e224d0003b3d1d7dddd6153
sasuke914 <takedaharuhito0914@gmail.com>
2024-09-30 13:15:07 -0700

    css

--- src/components/ui/font.tsx
+++ src/components/ui/font.tsx
@@ -1,4 +1,5 @@
-import { Inter, Russo_One } from 'next/font/google'
+import { Inter, Russo_One, Mulish } from 'next/font/google'
 
 export const inter = Inter({ subsets: ['latin'] });
 export const russo_one = Russo_One({ subsets: ['latin'], weight: ['400'] });
+export const mulish = Mulish({ subsets: ['latin'], weight: ['400'] });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,52 @@
+import { nextui } from "@nextui-org/react";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1440px",
+      },
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "var(--primaryBgColor)",
+        foreground: "var(--primaryWhiteColor)",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) -2px)",
+        sm: "calc(var(--radius) -4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-animate"),
+    nextui(),
+    require("tailwindcss-scrollbar")
+  ],
 };
 export default config;
